7ebbcd781870138061b5988d9a82b369a4bb5eff
paulcnl <paul.canarelli.2504@gmail.com>
2024-11-21 16:22:08 +0100

    debug-app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import { Outlet } from "react-router-dom";
 import { useEffect } from "react";
+import { Outlet } from "react-router-dom";
 import { getAllBooks } from "./components/data/getAllBooks";
 
 export default function App() {
