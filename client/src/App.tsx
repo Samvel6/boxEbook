0e060df77f6e47fc00cdf377180e6817ed042862
Samvel6 <samvel.saroukhanyan@gmail.com>
2024-11-22 12:06:20 +0100

    finished

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
 import "./App.css";
-import Donatepage from "./pages/Donatepage";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Donatepage from "./pages/Donatepage";
 
 export default function App() {
   return (
     <>
       <Header />
-      <Donatepage />
       <Outlet />
+      <Donatepage />
     </>
   );
 }
