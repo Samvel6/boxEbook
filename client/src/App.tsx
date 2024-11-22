82465f39cdc8f5e0850a6b70e2f94db1a84b337e
paulcnl <paul.canarelli.2504@gmail.com>
2024-11-22 12:31:25 +0100

    update main & header

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,11 @@ import "./App.css";
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header/Header";
 
-
 export default function App() {
   return (
     <>
       <Header />
       <Outlet />
-
     </>
   );
 }
