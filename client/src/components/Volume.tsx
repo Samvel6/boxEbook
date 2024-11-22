0aa3c23db2bc89f256529d1b75a5346ebe75f512
malachai <malachaimorrison@gmail.com>
2024-11-22 09:22:36 +0100

    to get the rest of the code

--- client/src/components/Volume.tsx
+++ client/src/components/Volume.tsx
@@ -12,7 +12,7 @@ function Volume() {
   const data = useLoaderData() as VolumeProps;
   console.info(data);
   return (
-    <>
+    <div className="volume">
       <div className="volume-container">
         <img className="volume-img" src={data.cover} alt="volume-cover" />
         <div className="volume-text">
@@ -22,7 +22,7 @@ function Volume() {
           <h2 className="volume-h5">{data.author}</h2>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
